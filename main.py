525d596133cafbf5837f99027c52304c3c49b5e4
thesticky009 <85288897+thesticky009@users.noreply.github.com>
2021-06-15 10:22:22 +0530

    main.py
    import discord
    from discord.ext import commands
    
    client = commands.Bot(command_prefix="!")
    
    @client.event
    async def on_ready():
      print("Bot Ready")
      
    @client.command()
    async def echo(ctx):
      await ctx.send("Hello World")
      
    @client.event
    async def on_ready():
      await client.change_presence(status=discord.Status.idle, activity=discord.Game("Hello World"))
    
    client.run("Bot Token")

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+  print("Bot Ready")
+  
+@client.command()
+async def echo(ctx):
+  await ctx.send("Hello World")
+  
+@client.event
+async def on_ready():
+  await client.change_presence(status=discord.Status.idle, activity=discord.Game("Hello World"))
+
+client.run("Bot Token")
